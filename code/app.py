faa6083337ad813a4ba79effe64328f3c7f729e0
Aitugan <isarbassov@gmail.com>
2022-03-01 21:21:28 +0600

    minor changes

--- code/app.py
+++ code/app.py
@@ -9,59 +9,60 @@ from .exceptions import ValidationException
 from .settings import VitrinaConfig
 from .extapi import send_search, get_status, rates_api, get_list, get
 import uuid
+#from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 
-app = Sanic("vitrina-service", config=VitrinaConfig())
+app = Sanic('vitrina-service', config=VitrinaConfig())
 
 
-@app.listener("before_server_start")
+@app.listener('before_server_start')
 async def init_before(app, loop):
     #app.ctx.db_pool = await asyncpg.create_pool(dsn=app.config.DATABASE_URL)
     app.ctx.redis = await aioredis.from_url(app.config.REDIS_URL, decode_responses=True, max_connections=50)
 
 
-@app.listener("after_server_stop")
+@app.listener('after_server_stop')
 async def cleanup(app, loop):
     await app.ctx.redis.close()
 
 
-@app.post("/search")
+@app.post('/search')
 async def post_search(request):
     req = request.json
     id = str(uuid.uuid4())
     await send_search(id, req, app.ctx.redis)
-    return response.json({"id":id})
+    return response.json({'id':id})
 
 
 # Search endpoint (GET, POST)
-@app.get("/search/<search_id>")
+@app.get('/search/<search_id>')
 async def get_search(request, search_id):
     status = await get_status(search_id, app.ctx.redis)
-    offers = await get_list(app.ctx.redis, search_id)
-    if not offers:
-        raise NotFound("Поиск с таким id не найден!")
+    if not status:
+        raise NotFound('Поиск с таким id не найден!')
+    offers = await get_list(search_id, app.ctx.redis)
     return response.json({
-        "search_id": search_id,
-        "status": status,
-        "items": offers
+        'search_id': search_id,
+        'status': status,
+        'items': offers
         })
 
 
-@app.get("/offers/<offer_id>")
+@app.get('/offers/<offer_id>')
 async def get_offer(request, offer_id):
-    offer = await get(app.ctx.redis, offer_id)
+    offer = await get(offer_id, app.ctx.redis)
     if not offer:
-        raise NotFound("Оффер с таким id не найден!")
-    return response.json({"pepsi": json.loads(offer)})
+        raise NotFound('Оффер с таким id не найден!')
+    return response.json({'pepsi': json.loads(offer)})
 
 
 # Booking endpoint (GET, POST)
-@app.get("/booking")
+@app.get('/booking')
 async def get_booking(request):
     return response.json(bookingResponse1)
 
 
-@app.post("/booking")
+@app.post('/booking')
 async def post_booking(request):
     req = request.json
     # Валидация обязательных параметров
@@ -69,7 +70,7 @@ async def post_booking(request):
     return response.json(bookingResponse2)
 
 
-@app.get("/get_rates")
+@app.get('/get_rates')
 async def get_rates(request):
     date = request.args.get('date')
     return await rates_api(date)
@@ -79,9 +80,9 @@ async def get_rates(request):
 async def validation_handler(request, e: ValidationException):
     return response.json(
             {
-                "detail": {
-                    "msg": str(e),
-                    "field": e.extra
+                'detail': {
+                    'msg': str(e),
+                    'field': e.extra
                 }
             },
             status=e.status_code)
@@ -91,9 +92,9 @@ async def validation_handler(request, e: ValidationException):
 async def notfound_handler(request, e: NotFound):
     return response.json(
         {
-            "detail": [
+            'details': [
                 {
-                    "msg": str(e)
+                    'msg': str(e)
                 }
             ]
         }
@@ -104,14 +105,14 @@ async def notfound_handler(request, e: NotFound):
 async def nexception_handler(request, e: SanicException):
     return response.json(
         {
-            "detail": [
+            'details': [
                 {
-                    "msg": str(e)
+                    'msg': str(e)
                 }
             ]
         }
     )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=app.config.DEBUG)
--- code/extapi.py
+++ code/extapi.py
@@ -24,14 +24,14 @@ async def send_search(id, req, redis):
 async def post2api(id, req, redis, provider):
     async with httpx.AsyncClient() as client:
         req['provider'] = provider
-        resp = None
+        #resp = None
         try:
             resp = await client.post(
-                "https://avia-api.k8s-test.aviata.team/offers/search", 
+                'https://avia-api.k8s-test.aviata.team/offers/search', 
                 json=req, 
                 timeout=PROVIDER_TIMEOUT)
             for item in resp.json()['items']:
-                offer_id = item["id"]
+                offer_id = item['id']
                 async with redis.pipeline(transaction=True) as pipe:
                     await pipe.lpush(f'vitrina.search:{id}', json.dumps(item))\
                         .set(f'vitrina.search:{offer_id}', json.dumps(item), ex=REDIS_KEY_EXPIRE)\
@@ -41,24 +41,26 @@ async def post2api(id, req, redis, provider):
 
 
 async def set_status(id, status, redis, provider):
-    await redis.set(f"vitrina.search:{provider}:STATUS:{id}", status, ex=REDIS_KEY_EXPIRE)
+    await redis.set(f'vitrina.search:{provider}:STATUS:{id}', status, ex=REDIS_KEY_EXPIRE)
 
 
 async def get_provider_status(id, redis, provider):
-    return await redis.get(f"vitrina.search:{provider}:STATUS:{id}")
+    return await redis.get(f'vitrina.search:{provider}:STATUS:{id}')
 
 
 async def get_status(id, redis):
     statuses = await asyncio.gather(*(get_provider_status(id, redis, p) for p in PROVIDERS))
+    if None in statuses:
+        return
     return 'DONE' if set(statuses) == {'DONE'} else 'PENDING'
 
 
-async def get_list(redis, id):
+async def get_list(id, redis):
     offers = await redis.lrange(f'vitrina.search:{id}', 0, -1)
     return [json.loads(offer) for offer in offers]
 
 
-async def get(redis, id):
+async def get(id, redis):
     return await redis.get(f'vitrina.search:{id}')
 
 
@@ -68,14 +70,14 @@ async def rates_api(d):
     except Exception as e:
         raise SanicException(str(e))
     if dt > date.today():
-        raise ValidationException("Дата должна быть раньше или равна сегодняшней")
+        raise SanicException('Дата должна быть раньше или равна сегодняшней')
     else:
         dt = date.today()
     async with httpx.AsyncClient() as client:
-        url = f"https://www.nationalbank.kz/rss/get_rates.cfm?fdate={dt.strftime('%d.%m.%Y')}"
+        url = f'https://www.nationalbank.kz/rss/get_rates.cfm?fdate={dt.strftime("%d.%m.%Y")}'
         resp = await client.get(url)
         if resp.status_code != 200:
-            raise SanicException("Не удалось получить данные от провайдера")
+            raise SanicException('Не удалось получить данные от провайдера')
         xml = xmltodict.parse(resp.text)
         rates = {rate['title']: rate['description'] for rate in xml['rates']['item']}
         return response.json({
